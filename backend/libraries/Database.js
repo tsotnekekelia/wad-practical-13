be37d611aee5ef02ffe43bc96860a7c2ebcbd272
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2019-11-25 11:30:18 +0200

    Added migration

--- backend/libraries/Database.js
+++ backend/libraries/Database.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.resolve(__dirname, '../db/todo.db');
 
-module.exports = new sqlite3.Database('./db/todo.db', sqlite3.OPEN_READWRITE, (err) => {
+module.exports = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
-})
+});
