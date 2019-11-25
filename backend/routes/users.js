b072077df78fd1b2829a6058e7c6096ba1aeead0
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2019-11-25 11:43:54 +0200

    Added solution

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const db = require('../libraries/Database')
 
 /* GET users tasks listing. */
 router.get('/1/tasks', function (req, res, next) {
-
-    fs.readFile('./data/tasks.json', (err, data) => {
-        if (err) throw err;
-        let tasks = JSON.parse(data);
-        res.json(tasks);
+    let sql = `SELECT * FROM tasks WHERE userId=? ORDER BY id DESC`;
+    db.all(sql, [1], (err, rows) => {
+        res.json(rows.map((task) => {
+            task.done = task.done > 0;
+            return task;
+        }));
     });
-
 });
 
 router.post('/1/tasks', function (req, res, next) {
 
    let title = req.body.title;
-    fs.readFile('./data/tasks.json', (err, data) => {
-        if (err) throw err;
-        let tasks = JSON.parse(data);
-
-        let item = {
-            id: tasks.length + 1,
-            title: title,
-            done: false
-        };
-        tasks.push(item);
-
-        fs.writeFile('./data/tasks.json', JSON.stringify(tasks), (err) => {
-            if (err) throw err;
-            res.json(item);
-        })
-    });
+   let sql = `INSERT INTO tasks(title, userId, done) VALUES(?, ?, ?);`;
+   db.run(sql,  ['Task #1', 1, 0],  function(err) {
+       if (err) throw err;
+       res.json({
+           id: this.lastID,
+           title: title,
+           userId: 1,
+           done: false
+       })
+   });
 });
 
 module.exports = router;
